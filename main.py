d9a50e4d95265bd69c0743e1bbff221d5058ea9d
Spyth <jens.dedeyne@gmail.com>
2024-02-08 20:12:39 +0100

    tried another way

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+from utils.file_utils import file_utils as uti
+from utils.openspace import OpenSpace as ospace
+from utils.table import Table
+
+
+list_openspace = []
+student_list = uti.read_file()
+
+BeCode = ospace()
+list_openspace = [BeCode]
+not_assigned = BeCode.assign_seats(student_list)
+BeCode.display()
+print(not_assigned)
+print()
+
+def write_file():
+    output_file = "./table_assignment.txt"
+    for table in list_openspace[0].tables:
+        for seat in table.seats:
+            with open(output_file, "a") as table_assigment:
+                table_assigment.write(f"{table.name} - {seat.name} - {seat.student}\n")
+write_file()
+
+
+
+
+
+
+
 
